87e5d128fab82f5d2f4d8707107173b052feaeee
Vladislav Laetansky <87545780+vlaetansky@users.noreply.github.com>
2022-08-14 22:53:02 +0300

    Adapt to our buildscript (#2)
    * Adapt to our buildscript
    
    * spotlessApply
    
    * Make it non-FML mod
    
    * spotlessApply
    
    * Change mod name corresponding to repository name

--- src/main/java/makamys/mixingasm/ConfigHelper.java
+++ src/main/java/makamys/mixingasm/ConfigHelper.java
@@ -9,67 +9,74 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-
+import net.minecraft.launchwrapper.Launch;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import net.minecraft.launchwrapper.Launch;
-
 public class ConfigHelper {
-    
+
     private final String MODID;
     private final Logger LOGGER;
-    
+
     public ConfigHelper(String modid) {
         this.MODID = modid;
         this.LOGGER = LogManager.getLogger(MODID);
     }
-    
+
     public Path getDefaultConfigFilePath(Path relPath) throws IOException {
-        String resourceRelPath = Paths.get("assets/" + MODID + "/default_config/").resolve(relPath).toString().replace('\\', '/');
-        URL resourceURL = new Object() { }.getClass().getEnclosingClass().getClassLoader().getResource(resourceRelPath);
-        
-        switch(resourceURL.getProtocol()) {
-        case "jar":
-            String urlString = resourceURL.getPath();
-            int lastExclamation = urlString.lastIndexOf('!');
-            String newURLString = urlString.substring(0, lastExclamation);
-            return FileSystems.newFileSystem(new File(URI.create(newURLString)).toPath(), null).getPath(resourceRelPath);
-        case "file":
-            return new File(URI.create(resourceURL.toString())).toPath();
-        default:
-            return null;
+        String resourceRelPath = Paths.get("assets/" + MODID + "/default_config/")
+                .resolve(relPath)
+                .toString()
+                .replace('\\', '/');
+        URL resourceURL =
+                new Object() {}.getClass().getEnclosingClass().getClassLoader().getResource(resourceRelPath);
+
+        switch (resourceURL.getProtocol()) {
+            case "jar":
+                String urlString = resourceURL.getPath();
+                int lastExclamation = urlString.lastIndexOf('!');
+                String newURLString = urlString.substring(0, lastExclamation);
+                return FileSystems.newFileSystem(new File(URI.create(newURLString)).toPath(), null)
+                        .getPath(resourceRelPath);
+            case "file":
+                return new File(URI.create(resourceURL.toString())).toPath();
+            default:
+                return null;
         }
     }
-    
+
     private void copyDefaultConfigFile(Path src, Path dest) throws IOException {
         Files.createDirectories(getParentSafe(dest));
         LOGGER.debug("Copying " + src + " -> " + dest);
         Files.copy(src, dest, StandardCopyOption.REPLACE_EXISTING);
     }
-    
+
     public boolean createDefaultConfigFileIfMissing(File configFile, boolean overwrite) {
         Path configFolderPath = Paths.get(new File(Launch.minecraftHome, "config").getPath());
         Path configFilePath = Paths.get(configFile.getPath());
 
         Path relPath = configFolderPath.relativize(configFilePath);
-        
+
         if (configFilePath.startsWith(configFolderPath)) {
             try {
                 Path defaultConfigPath = getDefaultConfigFilePath(relPath);
-                if(Files.isRegularFile(defaultConfigPath)) {
-                    if(!configFile.exists() || overwrite) {
+                if (Files.isRegularFile(defaultConfigPath)) {
+                    if (!configFile.exists() || overwrite) {
                         copyDefaultConfigFile(defaultConfigPath, configFile.toPath());
                     }
-                } else if(Files.isDirectory(defaultConfigPath)) {
+                } else if (Files.isDirectory(defaultConfigPath)) {
                     Files.createDirectories(Paths.get(configFile.getPath()));
                     // create contents of directory as well
-                    for(Object po : Files.walk(defaultConfigPath).toArray()) {
-                        Path destPath = configFile.toPath().resolve(
-                                defaultConfigPath.toAbsolutePath().relativize(((Path)po).toAbsolutePath()).toString());
-                        if(Files.isRegularFile((Path)po)) {
-                            if(!Files.exists(destPath) || overwrite) {
-                                copyDefaultConfigFile((Path)po, destPath);
+                    for (Object po : Files.walk(defaultConfigPath).toArray()) {
+                        Path destPath = configFile
+                                .toPath()
+                                .resolve(defaultConfigPath
+                                        .toAbsolutePath()
+                                        .relativize(((Path) po).toAbsolutePath())
+                                        .toString());
+                        if (Files.isRegularFile((Path) po)) {
+                            if (!Files.exists(destPath) || overwrite) {
+                                copyDefaultConfigFile((Path) po, destPath);
                             }
                         }
                     }
@@ -85,9 +92,9 @@ public class ConfigHelper {
         }
         return true;
     }
-    
+
     public Path getParentSafe(Path p) {
-        if(p == null || p.getParent() == null) {
+        if (p == null || p.getParent() == null) {
             return Paths.get("");
         } else {
             return p.getParent();
--- src/main/java/makamys/mixingasm/MixinConfigPlugin.java
+++ src/main/java/makamys/mixingasm/MixinConfigPlugin.java
@@ -1,19 +1,14 @@
 package makamys.mixingasm;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
 public class MixinConfigPlugin implements IMixinConfigPlugin {
-    
-    public static final String MODID = "mixingasm";
-    public static final Logger LOGGER = LogManager.getLogger(MODID);
-    
+
     @Override
     public void onLoad(String mixinPackage) {
         Mixingasm.run();
@@ -30,23 +25,16 @@ public class MixinConfigPlugin implements IMixinConfigPlugin {
     }
 
     @Override
-    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
-        
-    }
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {}
 
     @Override
     public List<String> getMixins() {
-        return Arrays.asList();
+        return Collections.emptyList();
     }
 
     @Override
-    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-        
-    }
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
 
     @Override
-    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-        
-    }
-
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
 }
